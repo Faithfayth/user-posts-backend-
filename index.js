696468ef42bfd6a92158e2f17352f7660db552f9
Faithfayth <169154594+Faithfayth@users.noreply.github.com>
2026-05-07 23:29:50 -0800

    user route connected, successful connection in postman

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ const connectDB = require('./db'); //importing the connectDB function from the d
 
 require('dotenv').config(); //loading environment variables from a .env file into process.env, allowing us to use environment variables in our application
 
+const userRoutes = require('./routes/users'); //importing the user routes from the routes/user.js file to handle user-related API endpoints
+
 const app = express();
+app.use(express.json()); //middleware to parse incoming JSON requests, allowing us to access the request body as req.body
 
 app.get('/', (req, res) => {
     res.send('Welcome to the User Posts Backend!');  //creating a route
-})
+});
+
+
+//user routes
+app.use('/users', userRoutes); //using the user routes for any requests that start with /users, so that we can handle user registration and login
 
 
 const PORT = process.env.PORT || 3000;  //setting up the port for the server to listen on, using an environment variable or defaulting to 3000
 
 app.listen(PORT, () => {
-    console.log(`Server is running at https://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 // now we have to connect the database
